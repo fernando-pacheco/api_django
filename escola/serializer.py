29aa3b8daf3df5322e4eee878edb629595189757
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2023-12-28 18:44:20 -0300

    autenticação para acesso dos dados

--- escola/serializer.py
+++ escola/serializer.py
@@ -6,14 +6,28 @@ class AlunoSerializer(serializers.ModelSerializer):
         model = Aluno
         fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento'] # '__all__'
 
-
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
 
-
 class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
         exclude = ['id']
+
+class ListaMatriculaAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+
+    def get_periodo(self, object):
+        return object.get_periodo_display()
+    
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno_nome = serializers.ReadOnlyField(source='aluno.nome')
+    class Meta:
+        model = Matricula
+        fields = ['aluno_nome']
